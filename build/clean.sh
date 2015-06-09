1aadc278d51f48f80a1cd40ac3c006fb737b7e60
Chris Rogers <rogersc@ainfosec.com>
2015-06-09 15:08:42 -0400

    Also reset the git mirrors on build
    Signed-off-by: Chris Rogers <rogersc@ainfosec.com>

--- build/clean.sh
+++ build/clean.sh
@@ -4,3 +4,14 @@ umask 0022
 rm -rf /tmp/bbfiles
 rm -rf build
 mkdir build
+
+#Reset git mirrors to stock
+
+mv git git.old
+mkdir git
+for repo in `ls git.old`; do
+    git clone --mirror git://github.com/openxt/$repo git/$repo
+done
+
+rm -rf git.old
+
